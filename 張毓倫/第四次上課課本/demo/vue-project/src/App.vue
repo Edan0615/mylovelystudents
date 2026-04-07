64f0a240d983b42170dbded620f02ab24e65689e
弦斷 <34684707+Edan0615@users.noreply.github.com>
2026-04-07 14:13:48 +0800

    2026 04 07 14 13

---
+++ 張毓倫/第四次上課課本/demo/vue-project/src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <my-info :is_happy="true"></my-info>
+</template>
+
+<style scoped></style>
---
+++ 張毓倫/第四次上課課本/demo/vue-project/src/components/MyInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :class="['btn', is_happy ? 'btn-success' : 'btn-danger']">
+    {{ is_happy ? '我很快樂' : '我不開心' }}
+  </button>
+  
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+// 定義 Props，建議加上型別限制更專業
+const props = defineProps({
+  is_happy: {
+    type: Boolean,
+    default: false
+  }
+});
+
+onMounted(() => {
+  console.log("組件已掛載，目前心情狀態：", props.is_happy);
+});
+</script>
